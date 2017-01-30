54a3dcd5fb1476656ce5c5248801f5715c64574b
QuentinR <quentin.rouhier@gmail.com>
2017-01-30 14:53:08 +0100

    liste de chiffre classer par pair

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ex3</title>
+  </head>
+  <body>
+    <?php
+    $min = 0;
+  $max = 300;
+    echo "Les chiffres pairs compris entre 0 et 300 sont:";
+
+   for($i = $min; $i <= $max; $i++) {
+      if ( $i % 2 == 1 ) {
+    echo "<br/>"."$i";
+      }
+  }
+  ?>
+  </body>
+</html>
